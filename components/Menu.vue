ea38793e414e05df6a16583cb6491fc1d6ea41e7
Citlalli <citlalli86@gmail.com>
2022-08-10 23:45:02 -0500

    avance 5%... falta mucho

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,7 +1,51 @@
 <template>
-  <h1>MENU</h1>
+<div class="menubar flex items-center justify-between">
+    <div class="brand mr-2 p-3">
+       <a href="//ethos.org.mx" target="_blank"> <img src="../assets/images/logo-dark.png" alt="logo ethos" class="logo" /></a>
+    </div>
+    <div class="nav flex items-center justify-end">
+      <ul class="navbar flex items-center justify-between">
+        <li><a href="">INICIO</a></li>
+        <li><a href="">ETHOS</a></li>
+        <li><a href="">EJES DE INVESTIGACIÓN</a></li>
+        <li><a href="">UNIVERSO THINK TANKS</a></li>
+        <li><a href="">HISTORIAS DE ÉXITO</a></li>
+      </ul>
+      <div class="search px-6">
+        <img src="../assets/images/lupa.png" alt="logo ethos" class="search-img" />
+      </div>
+      <img src="../assets/images/bars.png" alt="logo ethos" class="bars px-6" />
+    </div>
+
+</div>
 </template>
-
-<style>
-
+<script>
+export default {
+  components:{},
+  props: {
+    mode: {
+      type: String,
+      default: 'light'
+    }
+  },
+  data () {
+    return {
+      selectedIndex: 0, // the index of the selected tab,
+      tabs: []         // all of the tabs
+    }
+  },
+}
+</script>
+<style lang="scss">
+.menubar{
+  // position: relative;
+  &:after{
+    content: "";
+    position: absolute;
+    bottom: 5px;
+    width: 100%;
+    height: 2px;
+    background-color: #F28F78;
+  }
+  }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <Menu />
-    <Header />
     <Nuxt />
     <Footer />
   </div>
