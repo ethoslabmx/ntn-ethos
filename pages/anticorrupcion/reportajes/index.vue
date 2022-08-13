cc4a2f4f3173eb6fd5fafa3aa1a34216974cdba0
avaco <adrian@conect.mx>
2022-08-13 14:41:14 -0500

    ajustes secciones anticorrupcion

--- pages/anticorrupcion/reportajes/index.vue
+++ pages/anticorrupcion/reportajes/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <h1>Reportajes</h1>
+  <div>
+    <h1>Reportajes</h1>
+  <div>
+      <li v-for="evento of eventos" :key="evento.slug">
+          <NuxtLink :to="evento.slug">{{ evento.title }}</NuxtLink>
+          <img :src="evento.img" alt="">
+          <p>{{evento.body.children[1].children[0].value}}</p>
+        </li>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content }) {
+    const eventos = await $content("reportajes").fetch();
+
+    return {
+      eventos,
+    };
+  },
+}
 </script>
--- pages/anticorrupcion/videos/index.vue
+++ pages/anticorrupcion/videos/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <h1>Videos</h1>
+  <div>
+    <h1>Videos</h1>
+  <div>
+      <li v-for="evento of eventos" :key="evento.slug">
+          <NuxtLink :to="evento.slug">{{ evento.title }}</NuxtLink>
+
+          <p>{{evento.body.children[1].children[0].value}}</p>
+        </li>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content }) {
+    const eventos = await $content("videos").fetch();
+
+    return {
+      eventos,
+    };
+  },
+}
 </script>
