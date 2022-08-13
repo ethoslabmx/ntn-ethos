72eb9d9dc37d00a20ec593f29e430df971f200c2
avaco <adrian@conect.mx>
2022-08-13 14:35:06 -0500

    ajustes articulos

--- pages/anticorrupcion/noticias/index.vue
+++ pages/anticorrupcion/noticias/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <h1>Noticias</h1>
+  <div class="columnas">
+    <h1>Columnas de opinion</h1>
+    <div>
+      <li v-for="col of columnas" :key="col.slug">
+          <NuxtLink :to="col.slug">{{ col.title }}</NuxtLink>
+          <img :src="col.img" alt="">
+          <p>{{col.body.children[1].children[0].value}}</p>
+        </li>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content }) {
+    const columnas = await $content("noticias").fetch();
+
+    return {
+      columnas,
+    };
+  },
+}
 </script>
