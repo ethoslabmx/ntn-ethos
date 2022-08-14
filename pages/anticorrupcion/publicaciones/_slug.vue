2c56def1d2c4b7c6f2a8388f5de9980848f62db0
Citlalli <citlalli86@gmail.com>
2022-08-14 18:37:27 -0500

    check

--- pages/anticorrupcion/publicaciones/_slug.vue
+++ pages/anticorrupcion/publicaciones/_slug.vue
@@ -6,9 +6,7 @@
       <div class="w-full lg:w-3/4 p-5">
         <h1 class="2xl:text-xl text-lg title">{{ post.title }}</h1>
         <p class="my-6 font-bold">Fecha de publicación: {{ post.date }}</p>
-        <p class="my-6">Por: {{ post.autor }} <span v-if="post.medio">Para: <a :href="post.link">{{ post.medio
-        }}</a></span>
-        </p>
+        <p class="my-6">Por: {{post.autor}} <span v-if="post.medio">Para: <a :href="post.link">{{post.medio}}</a></span></p>
       </div>
     </div>
     <div class="flex container xl:px-0">
