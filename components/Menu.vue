4b178863472aae1b0de317b7e2f761a5beacc4e9
Citlalli <citlalli86@gmail.com>
2022-08-14 18:10:36 -0500

    avances articulo

--- components/Menu.vue
+++ components/Menu.vue
@@ -4,11 +4,11 @@
       <a href="//ethos.org.mx" target="_blank"> <img src="../assets/images/logo-dark.png" alt="logo ethos"
           class="logo" /></a>
     </div>
-    <div class="nav hidden xl:flex items-center justify-end ">
-      <ul class="navbar flex items-center justify-between">
+    <div class="nav  items-center justify-end flex">
+      <ul class="navbar  items-center justify-between hidden xl:flex">
         <li><a href="/">INICIO</a></li>
         <li class="dropdown inline-block relative">
-          <a class="font-semibold rounded inline-flex items-center">
+          <a class=" rounded inline-flex items-center">
             <span class="mr-1">ETHOS</span>
             <svg class="fill-current h-4 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
               <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
@@ -38,7 +38,7 @@
           </ul>
         </li>
         <li class="dropdown inline-block relative">
-          <a class="font-semibold rounded inline-flex items-center">
+          <a class=" rounded inline-flex items-center">
             <span class="mr-1">EJES DE INVESTIGACIÓN</span>
             <svg class="fill-current h-4 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
               <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
@@ -46,7 +46,7 @@
           </a>
           <ul class="dropdown-menu absolute hidden text-gray-700  bg-white shadow drop-shadow-sm p-0 m-0">
             <li class="dropdown-child inline-block relative">
-              <a class="font-semibold rounded inline-flex items-center py-3 px-6 w-full bg-gray-200 hover:bg-primary">
+              <a class=" rounded inline-flex items-center py-3 px-6 w-full bg-gray-200 hover:bg-primary">
                 <span class="mr-1">ANTICORRUPCIÓN</span>
                 <svg class="fill-current h-4 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                   <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
@@ -97,7 +97,7 @@
         </li>
 
         <li class="dropdown inline-block relative">
-          <a class="font-semibold rounded inline-flex items-center">
+          <a class=" rounded inline-flex items-center">
             <span class="mr-1">UNIVERSO THINK TANKS</span>
             <svg class="fill-current h-4 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
               <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
@@ -127,7 +127,7 @@
       <div class="search px-6">
         <img src="../assets/images/lupa.png" alt="logo ethos" class="search-img" />
       </div>
-      <img src="../assets/images/bars.png" alt="logo ethos" class="bars px-6" />
+      <img src="../assets/images/bars.png" alt="logo ethos" class="bars px-6 xl:hidden" />
     </div>
 
   </div>
--- pages/anticorrupcion/publicaciones/_slug.vue
+++ pages/anticorrupcion/publicaciones/_slug.vue
@@ -1,14 +1,37 @@
 <template>
-  <div>
-    <h1>{{post.title}}</h1>
-    <div class="col">
-      <p>Fecha de publicación: {{post.date}}</p>
-      <p>Por: {{post.autor}} <span v-if="post.medio">Para: <a :href="post.link">{{post.medio}}</a></span></p>
-      <nuxt-content :document="post" />
+  <div class="post_content py-28 bg-white">
+
+    <div class="container xl:px-0 flex ">
+      <div class="w-full lg:w-3/4 p-5">
+        <h1 class="2xl:text-xl text-lg title">{{ post.title }}</h1>
+        <p class="my-6 font-bold">Fecha de publicación: {{ post.date }}</p>
+        <p class="my-6">Por: {{ post.autor }} <span v-if="post.medio">Para: <a :href="post.link">{{ post.medio
+        }}</a></span>
+        </p>
+      </div>
     </div>
-    <div class="col">
-      <img :src="post.img" alt="">
-      <div class="btn">Descargar</div>
+    <div class="flex container xl:px-0">
+      <div class="w-full lg:w-3/4 p-5">
+
+        <nuxt-content :document="post" />
+
+        <div class="share my-20 content-end flex justify-end lg:items-center md:flex-wrap xl:flex-nowrap">
+
+          <small class="title mb-3 xl:mb-0">COMPARTIR</small>
+          <ul class="flex items-center ml-3 flex-nowrap">
+            <li class="mr-3"><a href=""><img src="../../../assets/images/linkedin-gray.png" alt="" class="h-5 w-5"></a>
+            </li>
+            <li class="mr-3"><a href=""><img src="../../../assets/images/fbb-gray.png" alt="" class="h-5 w-5"></a>
+            </li>
+            <li class="mr-3"><a href=""><img src="../../../assets/images/twwt-gray.png" alt="" class="v"></a></li>
+            <li class=""><a href=""><img src="../../../assets/images/letter.png" alt="" class="h-5 w-5"></a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="w-full lg:w-1/4 px-5">
+        <img :src="post.img" alt="">
+        <button class="btn ml-auto my-3 block underline">Descargar</button>
+      </div>
     </div>
   </div>
 </template>
