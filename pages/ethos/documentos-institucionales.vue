f4890f5eb61f999187f7852326a017a0b585e0f2
avaco <adrian@conect.mx>
2022-08-14 15:08:24 -0500

    documentos institucionales

--- pages/ethos/documentos-institucionales.vue
+++ pages/ethos/documentos-institucionales.vue
@@ -1,7 +1,31 @@
 <template>
-  <h1>Documentos Institucionales</h1>
+  <div>
+    <div class="hero">
+      <h1>Documentos Institucionales</h1>
+      <p>Encuentra aquí nuestras políticas organizacionales.</p>
+    </div>
+    <div class="documentos">
+      <div class="documento" v-for="d in documentos" :key="d.titulo">
+        <div class="col">
+          <h2>{{d.titulo}}</h2>
+          <p>{{d.autor}}</p>
+        </div>
+        <div class="col">
+          <img :src="d.img" alt="">
+          <a href="d.link">Descargar</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ $content }) {
+    const documentos= await $content("documentos").fetch();
+    return {
+      documentos
+    };
+  },
+}
 </script>
