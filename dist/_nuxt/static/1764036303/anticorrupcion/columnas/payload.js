c8123ee5e5329500b30e2b5b66ddba7761964799
ethoslabmx <111022982+ethoslabmx@users.noreply.github.com>
2025-11-27 08:51:05 -0600

    Delete dist directory

--- dist/_nuxt/static/1764036303/anticorrupcion/columnas/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/anticorrupcion/columnas", {data:[{columnas:[],total:[]}],fetch:{},mutations:[]});
--- dist/_nuxt/static/1764036303/desarrollo-sostenible/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/desarrollo-sostenible", {data:[{}],fetch:{},mutations:[]});
